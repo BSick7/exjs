992f6e3af96129aded3cd7445361d62e389274d5
BSick7 <brad.sickles@gmail.com>
2014-05-09 11:25:26 -0400

    Added ability to package exjs into nuget package.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
 ﻿module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-typescript');
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-shell');
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('./package.json'),
         typescript: {
             build: {
                 src: ['src/**/*.ts'],
@@ -28,9 +29,19 @@
         watch: {
             files: '**/*.ts',
             tasks: ['typescript:build']
+        },
+        shell: {
+            package: {
+                options: {
+                    stdout: true,
+                    stderr: true
+                },
+                command: 'powershell ./package.ps1 <%= pkg.version %>'
+            }
         }
     });
 
     grunt.registerTask('default', ['typescript:build']);
     grunt.registerTask('test', ['typescript:build', 'typescript:test', 'qunit']);
+    grunt.registerTask('package', ['shell:package']);
 };
---
+++ package.ps1
@@ -0,0 +1,23 @@
+param([string]$version)
+
+<#
+    Creates nuget package for exjs
+#>
+
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path $Invocation.MyCommand.Path
+}
+$root = Get-ScriptDirectory
+
+# Update version in nuspec file
+$specfile = $root + "\exjs.nuspec"
+[xml]$specxml = New-Object System.Xml.XmlDocument
+$specxml.PreserveWhitespace = $true
+$specxml.Load($specfile)
+$specxml.package.metadata.version = $version
+Set-Content $specfile $specxml.OuterXml
+
+# Package
+nuget pack $specfile -Version $version -OutputDirectory ".\nuget"
